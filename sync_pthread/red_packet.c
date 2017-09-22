e60b8a5ca574e061dd279851ec7430694e64f072
root <root@www.sn.10086.cn>
2017-09-22 19:23:04 +0800

    一些优化

--- sync_pthread/red_packet.c
+++ sync_pthread/red_packet.c
@@ -13,12 +13,13 @@
 #include<time.h>
 
 
-#define _RAND(min,max)(rand()%((max) - (min)) + min)
+#define RAND(min,max)(rand()%((max) - (min) +1) + min)
 
+char name[6];
 struct packet
 {
-    int num;
-    int sum;
+    int num; //红包个数
+    double sum;  //红包总额
 }Packet;
 
 
@@ -28,98 +29,122 @@ pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 
 void sys_err(char *s,int ret)
 {
-    fprintf(stderr,"%s error: %s\n",*s,strerror(ret));
+    fprintf(stderr,"%s error: %s\n",s,strerror(ret));
     exit(1);
 }
+
+//随机生成字符串
+char *prod_name(void)
+{
+    char max = 'z';
+    char min = 'a';
+
+    int i = 0;
+    for(i = 0;i < 5;i++)
+    {
+        name[i] = rand()%(max-min + 1) + min;
+    }
+
+    return name;
+}
+
+//主线程模拟发红包
 void producter()
 {
     int num;  //红包个数 
-    int sum;  //红包总钱数
+    double sum;  //红包总额
 
-    //while(1)
-    //{
+    while(1)
+    {
 
         printf("请输入红包的金额和个数：");
-        scanf("%d %d",&sum,&num);
-
-        pthread_mutex_lock(&mutex);
-        Packet.num = num;
-        Packet.sum = sum * 100;
-
-        pthread_cond_broadcast(&cond);
-        pthread_mutex_unlock(&mutex);
-
-        sleep(1);
-
-
-   // }
-}
-
-void *consumer(void *arg)
-{
-    int money = 0;
-    int i = (int) arg;
-
-    //while(1)
-    //{
-        pthread_mutex_lock(&mutex);
-        pthread_cond_wait(&cond,&mutex);
-        if(Packet.num <= 0)
+        scanf("%lf %d",&sum,&num);
+        if((sum *100)/num >= 1)
         {
-            printf("%dth thread\tget 0.00 money\n",i+1);
-            pthread_mutex_unlock(&mutex);
-            pthread_exit(NULL);
-         //   break;
-        }
-        else if(Packet.num == 1)
-        {
-            money = Packet.sum;
+            break;
         }
         else
         {
-            money = Packet.sum *_RAND(1,199)/100/Packet.num;
+            printf("请输入正确的金额或红包数，每个红包最少1分\n");
+            continue;
         }
 
-        Packet.sum -= money;
-        Packet.num--;
-        printf("%dth thread\tget %d.%0.2d money\n",i+1,money/100,money%100);
-        pthread_mutex_unlock(&mutex);
+    }
+    pthread_mutex_lock(&mutex);
+    Packet.num = num;
+    Packet.sum = sum * 100;
 
-    //}
+    pthread_cond_broadcast(&cond);
+    pthread_mutex_unlock(&mutex);
+
+    sleep(1);
+
+}
+
+//子线程模拟抢红包
+void *consumer(void *arg)
+{
+    int money = 0;
+
+    pthread_mutex_lock(&mutex);
+    pthread_cond_wait(&cond,&mutex);  //子线程阻塞等待主线程唤醒
+
+    //若红包以被抢完，则打印并退出
+    if(Packet.num <= 0)
+    {
+        printf("%s\tget 0.00 money\n",prod_name());
+        pthread_mutex_unlock(&mutex);
+        pthread_exit(NULL);
+    }
+    else if(Packet.num == 1) 
+    {
+        money = Packet.sum;  //若红包剩一个则直接给线程
+    }
+    else if(Packet.sum/Packet.num == 1)
+    {
+        money = 1;   //保证每个包最少一分
+    }
+    else
+    {
+        money = Packet.sum *RAND(1,199)/100/Packet.num; //随机分配金额
+    }
+
+    Packet.sum -= money;
+    Packet.num--;
+    printf("%s\tget %d.%d%d money\n",prod_name(),money/100,(money/10)%10,money%10);
+    pthread_mutex_unlock(&mutex);
+
 
     return NULL;
 }
 int main()
 {
     int ret;
-    pthread_t ctid[10];
+    pthread_t ctid[100];
     srand(time(NULL)); //设置随机数种子
 
 
     int i = 0;
-    for(i = 0;i < 10;i++)
+    //创建子线程
+    for(i = 0;i < 100;i++)
     {
-        ret = pthread_create(&ctid[i],NULL,consumer,(void *)i);
+        ret = pthread_create(&ctid[i],NULL,consumer,NULL);
         if(ret != 0)
         {
             sys_err("pthread_create",ret);
         }
 
     }
+    //主线程
     producter();
 
     
+    //回收
     for(i = 0;i <10;i++)
     {
         pthread_join(ctid[i],NULL);
     }
     
-    
-    
-
-    
- //   pthread_join(ptid,NULL);
-
     pthread_mutex_destroy(&mutex);
     pthread_cond_destroy(&cond);
 
