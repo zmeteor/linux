df69a1c77fabce4da949f91929296a6a26034203
ryan <z.ryan1024@gmail.com>
2018-06-30 20:46:37 +0800

    使用SIGURG通知带外数据

---
+++ signal/testSignal/testSIGURG/server.c
@@ -0,0 +1,99 @@
+/*************************************************************************
+	> File Name: server.c
+	> Author: 
+	> Mail: 
+	> Created Time: 2018年06月30日 星期六 13时11分14秒
+ ************************************************************************/
+//使用SIGURG检测带外数据是否到达
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <assert.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <signal.h>
+#include <fcntl.h>
+
+#define BUF_SIZE 1024
+#define port 8888
+
+static int connfd;
+
+void sig_urg( int sig )
+{
+    int save_errno = errno;
+    
+    char buffer[ BUF_SIZE ];
+    memset( buffer, '\0', BUF_SIZE );
+    int ret = recv( connfd, buffer, BUF_SIZE-1, MSG_OOB );
+    printf( "got %d bytes of oob data '%s'\n", ret, buffer );
+
+    errno = save_errno;
+}
+
+void addsig( int sig, void ( *sig_handler )( int ) )
+{
+    struct sigaction sa;
+    memset( &sa, '\0', sizeof( sa ) );
+    sa.sa_handler = sig_handler;
+    sa.sa_flags |= SA_RESTART;
+    sigfillset( &sa.sa_mask );
+    assert( sigaction( sig, &sa, NULL ) != -1 );
+}
+
+int main( int argc, char* argv[] )
+{
+
+    struct sockaddr_in address;
+    bzero( &address, sizeof( address ) );
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons( port );
+
+    int sock = socket( PF_INET, SOCK_STREAM, 0 );
+    assert( sock >= 0 );
+
+    int ret = bind( sock, ( struct sockaddr* )&address, sizeof( address ) );
+    assert( ret != -1 );
+
+    ret = listen( sock, 5 );
+    assert( ret != -1 );
+
+    struct sockaddr_in client;
+    socklen_t client_addrlength = sizeof( client );
+    connfd = accept( sock, ( struct sockaddr* )&client, &client_addrlength );
+    if ( connfd < 0 )
+    {
+        printf( "errno is: %d\n", errno );
+    }
+    else
+    {
+        addsig( SIGURG, sig_urg );
+        
+        //使用SIGURG信号之前，必须设置socket的宿主进程或进程组
+        fcntl( connfd, F_SETOWN, getpid() );
+
+        char buffer[ BUF_SIZE ];
+        while( 1 )  //循环读取普通数据
+        {
+            memset( buffer, '\0', BUF_SIZE );
+            ret = recv( connfd, buffer, BUF_SIZE-1, 0 );
+            if( ret <= 0 )
+            {
+                break;
+            }
+            printf( "got %d bytes of normal data '%s'\n", ret, buffer );
+        }
+
+        close( connfd );
+    }
+
+    close( sock );
+    return 0;
+}
+
+
